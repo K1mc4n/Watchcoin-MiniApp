fcf8496859efadf601eea688e5018803f84ca5e4
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-06-22 23:12:41 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,15 @@
+import "../globals.css";
+import { ReactNode } from "react";
+import { Header } from "../components/Header";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white min-h-screen font-sans">
+        <Header />
+        <main className="p-4 max-w-4xl mx-auto">{children}</main>
+      </body>
+    </html>
+  );
+}
 
